b6a0201fd0a92723bf67348a37ae1d49e51d718a
suryansh <suryanshpatwal@gmail.com>
2024-09-03 15:07:47 +0530

    fixed the images not loading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const Hero = () => (
-    <section id="home" className="bg-gray-900 text-white h-screen flex items-center justify-center bg-cover bg-center relative" style={{ backgroundImage: "url('./public/s4ds group.jpg')" }}>
+    <section id="home" className="bg-gray-900 text-white h-screen flex items-center justify-center bg-cover bg-center relative" style={{ backgroundImage: "url('/s4ds group.jpg')" }}>
         <div className="absolute inset-0 bg-black opacity-50 blur-sm"></div>
         <div className="z-10 text-center">
             <h1 className="text-5xl font-bold mb-4">Welcome to Our Society</h1>
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-  css: {
-    postcss: {
-      plugins: [
-        require('postcss-loader'),
-        require('pug-loader'),
-      ],
-    },
-  },
-});
+  plugins: [react()],
+})
